29898aad0abdd799d6d774b2969fcbd94c7952c4
Diego Navarro <code@diegognt.dev>
2023-02-21 16:01:08 -0500

    Listing the available posts on a page.

--- services/posts/index.test.ts
+++ services/posts/index.test.ts
@@ -1,23 +1,52 @@
-import { loadPost } from "./index.ts";
+import * as PostService from "./index.ts";
 import {
   assertEquals,
   assertInstanceOf,
   assertNotEquals,
   assertStringIncludes,
 } from "$std/testing/asserts.ts";
+import { assertSpyCalls, returnsNext, stub, spy, assertSpyCall } from "$std/testing/mock.ts";
+import { describe, it } from "$std/testing/bdd.ts";
 import { Post } from "./@types.ts";
 
-Deno.test("'loadPost()' return `null` if not found a post", async () => {
-    assertEquals(await loadPost("do-not-exists"), null);
+const emptyAsyncIterable = {
+  [Symbol.asyncIterator] : async function*() {}
+};
+
+describe("The 'loadPost()' function", () => {
+  it("returns `null` if slug does not exists.", async () => {
+    assertEquals(await PostService.loadPost("do-not-exists"), null);
+  });
+
+  it("return a `Post` when slug exists.", async () => {
+    //Act
+    const post: Post | null = await PostService.loadPost("hello-world");
+
+    // Assertions
+    assertNotEquals(post, null);
+    assertEquals(post?.slug, "hello-world");
+    assertStringIncludes(post?.title as string, "the first post");
+    assertInstanceOf(post?.date, Date);
+  });
 });
 
-Deno.test("'loadPost()' should return a `Post` when slug exists.", async () => {
-  //Act
-  const post: Post | null = await loadPost("hello-world");
+describe("The 'listPosts()' function", () => {
+  it("returns an empty array if there is no psots to list.", async () => {
+    const readDirStub = stub(Deno, "readDir", returnsNext([ emptyAsyncIterable ]));
+    try {
+      assertEquals(await PostService.listPosts(), []);
+    } finally {
+      readDirStub.restore();
+    }
 
-  // Assertions
-  assertNotEquals(post, null);
-  assertEquals(post?.slug, "hello-world");
-  assertStringIncludes(post?.title as string, "the first post");
-  assertInstanceOf(post?.date, Date);
+    assertSpyCall(readDirStub, 0, {
+    args: ["./content/posts/"],
+  });
+    assertSpyCalls(readDirStub, 1);
+  });
+
+  it("returns an array of `Post` if there is any", async () => {
+    const posts = await PostService.listPosts();
+    assertEquals(posts.length > 0, true);
+  });
 });
