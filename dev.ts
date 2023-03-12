7adc5fa5465b88b57c5e0e8c0a0b5f3e1151e812
Diego Navarro <code@diegognt.dev>
2023-03-12 09:23:22 -0500

    404 page

--- dev.ts
+++ dev.ts
@@ -1,4 +1,4 @@
-#!/usr/bin/env -S deno run -A --watch=static/,routes/
+#!/usr/bin/env -S deno run -A --watch=static/,routes/,components/
 
 import dev from "$fresh/dev.ts";
 
---
+++ routes/_404.tsx
@@ -0,0 +1,15 @@
+import { UnknownPageProps } from "$fresh/server.ts";
+import { Header } from "../components/Header.tsx";
+
+export default function NotFoundPage({ url }: UnknownPageProps) {
+  return (
+    <>
+      <Header />
+      <main class="px-6 pt-28 pb-12 flex flex-col justify-start items-center leading-normal bg-gray-50 h-screen">
+        <article class="w-5/6 mb-6 p-10 border border-gray-40 lg:border-gray-400 bg-white text-center rounded">
+          <p class="m-0 text-gray-900 text-2xl font-bold">404 not found: {url.pathname}</p>
+        </article>
+      </main>
+    </>
+  );
+}
