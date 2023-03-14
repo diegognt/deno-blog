61f3ce676833dfd8338c99d900193134eb5f8652
Diego Navarro <code@diegognt.dev>
2023-03-14 16:52:44 -0500

    Footer component, initial layout and content.

--- routes/_404.tsx
+++ routes/_404.tsx
@@ -1,9 +1,10 @@
 import { UnknownPageProps } from "$fresh/server.ts";
+import { Footer } from "../components/Footer/index.tsx";
 import { Header } from "../components/Header.tsx";
 
 export default function NotFoundPage({ url }: UnknownPageProps) {
   return (
-    <>
+    <body>
       <Header />
       <main class="px-6 pt-28 pb-12 flex flex-col justify-start items-center leading-normal bg-gray-50 h-screen">
         <article class="w-5/6 mb-6 p-10 border border-gray-40 lg:border-gray-400 bg-white text-center rounded">
@@ -11,6 +12,7 @@ export default function NotFoundPage({ url }: UnknownPageProps) {
           <a class="text-gray-900 cursor-pointer hover:underline" href="/">Return to the list of posts</a>
         </article>
       </main>
-    </>
+      <Footer />
+    </body>
   );
 }
--- routes/posts/[id].tsx
+++ routes/posts/[id].tsx
@@ -4,7 +4,8 @@ import { Post } from "../../services/posts/index.types.ts";
 import { loadPost } from "../../services/posts/index.ts";
 import { CSS } from "$gfm/mod.ts";
 import { Head } from "$fresh/runtime.ts";
-import {Header} from "../../components/Header.tsx"
+import { Header } from "../../components/Header.tsx";
+import { Footer } from "../../components/Footer/index.tsx";
 
 export const handler: Handlers = {
   async GET(_request: Request, context: HandlerContext) {
@@ -27,21 +28,28 @@ export default function PostPage(props: PageProps): JSX.Element {
         <meta name="description" content={post.excerpt} />
         <style dangerouslySetInnerHTML={{ __html: CSS }} />
       </Head>
-      <Header />
-      <main class="px-6 pt-28 pb-12 flex flex-col leading-normal bg-gray-50 h-screen">
-        <article class="p-6 mb-6 border border-gray-40 lg:border-gray-400 bg-white rounded" itemScope itemType="http://schema.org/BlogPosting">
-          <header class="mb-6">
-            <h1 itemProp="headline" class="text-4xl">{post.title}</h1>
-            <time itemProp="datePublished">
-              {Intl.DateTimeFormat().format(post.date)}
-            </time>
-          </header>
-          <div
-            class="markdown-body"
-            dangerouslySetInnerHTML={{ __html: post.body }}
-          />
-        </article>
-      </main>
+      <body class="bg-gray-50">
+        <Header />
+        <main class="container mx-auto px-4 md:px-8 pt-28 pb-12 flex flex-col grow leading-normal min-h-screen">
+          <article
+            class="p-6 border border-gray-40 lg:border-gray-400 bg-white rounded"
+            itemScope
+            itemType="http://schema.org/BlogPosting"
+          >
+            <header class="mb-6">
+              <h1 itemProp="headline" class="text-4xl">{post.title}</h1>
+              <time itemProp="datePublished">
+                {Intl.DateTimeFormat().format(post.date)}
+              </time>
+            </header>
+            <div
+              class="markdown-body"
+              dangerouslySetInnerHTML={{ __html: post.body }}
+            />
+          </article>
+        </main>
+        <Footer />
+      </body>
     </>
   );
 }
